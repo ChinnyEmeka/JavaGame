a3bbf18113a78b293561e1adc2afda68aa684996
cemeka <chinny.emeka1@gmail.com>
2015-12-16 12:41:05 -0500

    Robot now on screen

--- src/simplegame/StartingClass.java
+++ src/simplegame/StartingClass.java
@@ -3,34 +3,54 @@ package simplegame;
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Frame;
+import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.Graphics;
+import java.net.URL;
 
 public class StartingClass extends Applet implements Runnable, KeyListener {
-   public void init(){
+   private Robot robot; 
+   private Image image, character;
+   private URL base; //url allows us to use addresses (such as C:\\Users\\Desktop\\image1.jpg)
+   private Graphics second;
+   
+	public void init(){
 	   setSize(800, 400);
 	   setBackground(Color.BLACK);
 	   setFocusable(true);
 	   Frame frame = (Frame) this.getParent().getParent();
 	   frame.setTitle("Bot Alpha");
 	   addKeyListener(this);
+	   try{
+		   base = getDocumentBase();
+	   }catch(Exception e){
+		   //handle exception
+	   }
+	   
+	   //Image setups
+	   character = getImage(base, "data/character.png");
    }
    
    public void start(){
-	   super.start();
+	   robot = new Robot();
+	   
+	   Thread thread = new Thread(this);
+	   thread.start();
    }
    
    public void stop(){
-	   super.stop();
+	 
    }
    
    public void destroy(){
-	   super.destroy();
+	  
    }
    @Override
 	public void run() {
 		// TODO Auto-generated method stub
 	   while(true){
+		   robot.update();
 		   repaint();
 		   try{
 			   Thread.sleep(17);
@@ -41,6 +61,29 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 	   }
 		
 	}
+   
+   //the update method is used for double buffering. 
+   @Override
+	public void update(Graphics g) {
+		// TODO Auto-generated method stub
+		if (image == null){
+			image = createImage(this.getWidth(), this.getHeight());
+			second = image.getGraphics();
+		}
+		
+		second.setColor(getBackground());
+		second.fillRect(0,  0, getWidth(), getHeight());
+		second.setColor(getForeground());
+		paint(second);
+		
+		g.drawImage(image, 0, 0 , this);
+	}
+   
+   @Override
+	public void paint(Graphics g) {
+		// TODO Auto-generated method stub
+		g.drawImage(character, robot.getCenterX() - 61, robot.getCenterY() - 63, this);
+	}
 
 
 
@@ -49,7 +92,7 @@ public void keyPressed(KeyEvent e) {
 
     switch (e.getKeyCode()) {
     case KeyEvent.VK_UP:
-        System.out.println("Move up");
+    	System.out.println("Move up");
         break;
 
     case KeyEvent.VK_DOWN:
@@ -57,14 +100,17 @@ public void keyPressed(KeyEvent e) {
         break;
 
     case KeyEvent.VK_LEFT:
-        System.out.println("Move left");
+    	robot.moveLeft();
+     
         break;
 
     case KeyEvent.VK_RIGHT:
-        System.out.println("Move right");
+    	robot.moveRight();
+       
         break;
 
     case KeyEvent.VK_SPACE:
+    	robot.jump();
         System.out.println("Jump");
         break;
 
@@ -84,11 +130,11 @@ public void keyReleased(KeyEvent e) {
         break;
 
     case KeyEvent.VK_LEFT:
-        System.out.println("Stop moving left");
+        robot.stop();
         break;
 
     case KeyEvent.VK_RIGHT:
-        System.out.println("Stop moving right");
+    	robot.stop();
         break;
 
     case KeyEvent.VK_SPACE:
